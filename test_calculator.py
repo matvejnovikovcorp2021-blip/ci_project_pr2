7f22383ddbc5928eef20e800366ac91db80ac22b
matvejnovikovcorp2021-blip <matvejnovikov.corp.2021@gmail.com>
2026-04-14 21:39:35 +0300

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,4 @@
+import pytest
 from calculator import add
 
 def test_add():
@@ -6,3 +7,10 @@ def test_add():
     assert add(0,0) == 0
     assert add(5,5) == 10
     assert add(3,7) == 10
+
+@pytest.mark.parametrize("a,b,expected", [
+    (10,5,15)
+    (-10,-5,-15)
+    (1.5,2.5,4.0)])
+def test_add_complex(a,b,expected):
+    assert add(a,b) == expected
